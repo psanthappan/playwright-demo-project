a22425ab0fcc41ce1db444badb58019888b45930
Periyanayagam Santhappan <periyanayagam.santhappan@intellificial.com>
2025-06-17 20:51:48 +1000

    add data from json

--- tests/functional/login.pom.spec.ts
+++ tests/functional/login.pom.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../page-objects/login.page";
+import constants from "../../data/constants.json";
 
 test.describe("Login functionality", () => {
   // Successful login
   test("Should login successfully", async ({ page }) => {
+    // Reading data from constants
+    console.log(`>>> Data from constants file: ${JSON.stringify(constants)}`);
+
     const loginpage = new LoginPage(page); // Instance creation
     await loginpage.launchHomePage("https://katalon-demo-cura.herokuapp.com/");
-    await loginpage.login('John Doe', 'ThisIsNotAPassword');
+    await loginpage.login("John Doe", "ThisIsNotAPassword");
   });
 });
